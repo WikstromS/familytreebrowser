ec6c21585c9d5e4a32dc600b844d53352eab2617
Santeri Wikström <wikke93@gmail.com>
2017-03-21 23:09:38 +0200

    Add files via upload
    Started the project. Made handling with the json file and got a method that loops through every object.

---
+++ familytreebrowser/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace familytreebrowser
+{
+    class Program
+    {
+
+        public static string path = "C://Users//wiku//Desktop//OOP Coding Assignment//OOP Coding Assignment//familytrees.json";
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("First name is " + args.input);
+            Console.WriteLine("Last name is " + args[1]);
+
+            string json = File.ReadAllText(path);
+            dynamic file = JsonConvert.DeserializeObject(json);
+
+
+            //PrintKaikki(file);
+
+        }
+
+    public static void PrintKaikki(dynamic json)
+    {       
+            foreach(var obj in json)
+            {
+                Console.WriteLine(obj.FirsName + " " + obj.LastName + " " + obj.Age);
+                
+                // TODO luo taulukko, johon lisätään nimet ja iät
+
+                 if (obj.Children != null)
+                   PrintKaikki(obj.Children);
+            }
+
+        }
+    }
+
+}
