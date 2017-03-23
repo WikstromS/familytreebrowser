be08b60f4aea406d11851fa1b2d0afb58292a8c4
Santeri Wikström <wikke93@gmail.com>
2017-03-23 23:00:25 +0200

    Made some progress

--- familytreebrowser/Program.cs
+++ familytreebrowser/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using System.Collections;
 
 namespace familytreebrowser
 {
@@ -23,30 +24,37 @@ namespace familytreebrowser
         static void Main(string[] args)
         {
            
-
+/*
             if (args[0] == "-input")
-                path = args[1];
+                //path = args[1];
             if (args[0] == "-sort")
                 //sortcode
             if (args[0] == "-search")
                     //searchcode
             if (args[0] == "-findduplicateinfamilytree")
                     //duplicatecode
-                    { } 
+                     */
 
             string json = File.ReadAllText(path);
             dynamic file = JsonConvert.DeserializeObject(json);
+            //Deserialisoi objektiin ja Henkilo luokkaan. Koska Json tiedosto oli Arraymuodossa tarvitsin muodon Henkilo[]
+            var obj = JsonConvert.DeserializeObject<Henkilo[]>(json);
             
 
 
 
             PrintEveryOne(file);
             Console.WriteLine("---------------------");
-            
+            Console.WriteLine(obj[0].FirsName);
+            for(int i = 0; i <obj.Length;i++ )
+            {
+                Console.WriteLine(obj[i].FirsName);
+            }
 
-            
 
-            
+
+
+
 
         }
 
@@ -68,7 +76,21 @@ namespace familytreebrowser
         }
 
 
+ 
 
+
+
+
+
+
+    }
+    public class Henkilo
+    {
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public ICollection<Henkilo> Children { get; set; }
     }
 
 }
