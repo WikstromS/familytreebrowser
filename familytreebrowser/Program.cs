0579254dcdb67502b763df3959dd9d8a03ab3ff6
WikstromS <wikke93@gmail.com>
2017-03-24 09:49:59 +0200

    implemented rough sort and a list of Henkilo objects

--- familytreebrowser/Program.cs
+++ familytreebrowser/Program.cs
@@ -16,9 +16,10 @@ namespace familytreebrowser
     {
 
         public static string path = "familytrees.json";
- 
-    
-      
+        public static List<Henkilo> henkiloLista = new List<Henkilo>();
+
+
+
 
 
         static void Main(string[] args)
@@ -39,44 +40,89 @@ namespace familytreebrowser
             dynamic file = JsonConvert.DeserializeObject(json);
             //Deserialisoi objektiin ja Henkilo luokkaan. Koska Json tiedosto oli Arraymuodossa tarvitsin muodon Henkilo[]
             var obj = JsonConvert.DeserializeObject<Henkilo[]>(json);
-            
 
 
 
-            PrintEveryOne(file);
+
+            MakeAList(obj); //Luo listan
+            PrintEveryOne(obj); // tällä hetkellä PrintEveryOne metodissa lisätään kaikki listaan. TODO. Tee oma metodi, joka käy objektit läpi A'la PrintEveryOne ja lisää listaan.
             Console.WriteLine("---------------------");
-            Console.WriteLine(obj[0].FirsName);
+            Console.WriteLine(henkiloLista.Count);
+            //sorttaa lista by Age
+            List<Henkilo> sortedList = henkiloLista.OrderBy(o => o.Age).ToList();
+            foreach(var i in sortedList)
+            {
+                Console.WriteLine(i.Age);
+            }
+            
+            /*
             for(int i = 0; i <obj.Length;i++ )
             {
                 Console.WriteLine(obj[i].FirsName);
+                if (obj[i].Children != null)
+                {
+                    foreach (var ob in obj[i].Children)
+                    {
+                        Console.WriteLine(ob.FirsName);
+
+                        if (ob.Children != null)
+                        {
+                            foreach (var obb in ob.Children)
+                            {
+                                Console.WriteLine(obb.FirsName);
+
+                                if(obb.Children != null)
+                                {
+                                    foreach(var obbb in obb.Children)
+                                    {
+                                        Console.WriteLine(obbb.FirsName);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } 
+
             }
 
-
-
-
-
+                */
 
         }
 
-    public static void PrintEveryOne(dynamic json)
-    {
+    public static void PrintEveryOne(dynamic json) {
+        
                 foreach (var obj in json)
                 {
                     Console.WriteLine(obj.FirsName + " " + obj.LastName + " " + obj.Age);
 
-                // While looping through each object, i save the objects to sort later on
-                string values = obj.FirsName + obj.LastName + obj.Age + obj.Gender;
-              
-    
+
+
                  if (obj.Children != null)
                         PrintEveryOne(obj.Children);
+                
                 }
                 
             
         }
 
+        public static void MakeAList(dynamic json)
+        {
 
- 
+            foreach (var obj in json)
+            {
+
+                henkiloLista.Add(new Henkilo(obj.FirsName, obj.LastName, obj.Gender, obj.Age));
+
+                if (obj.Children != null)
+                    PrintEveryOne(obj.Children);
+
+            }
+
+
+        }
+
+
+
 
 
 
@@ -90,7 +136,15 @@ namespace familytreebrowser
         public string LastName { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
-        public ICollection<Henkilo> Children { get; set; }
+        public IList<Henkilo> Children { get; set; }
+
+        public Henkilo(string firstname, string lastname, string gender, int age)
+        {
+            this.FirsName = firstname;
+            this.LastName = lastname;
+            this.Gender = gender;
+            this.Age = age;
+        }
     }
 
 }
