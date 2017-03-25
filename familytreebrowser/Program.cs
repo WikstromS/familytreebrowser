30fbe965f0785b6b96701b29118060cd31c77639
Santeri Wikström <wikke93@gmail.com>
2017-03-25 10:39:51 +0200

    Fixed commandline parameteres and now only prints 10 lines at a time

--- familytreebrowser/Program.cs
+++ familytreebrowser/Program.cs
@@ -15,114 +15,133 @@ namespace familytreebrowser
     class Program
     {
 
-        public static string path = "";
+        public static string path = "familytrees.json";
         public static List<Henkilo> henkiloLista = new List<Henkilo>();
-
-
-
 
 
         static void Main(string[] args)
         {
+            /*
+            string json = File.ReadAllText(path);       //Reads the JSON file to a string
+            var obj = JsonConvert.DeserializeObject<Henkilo[]>(json);
+            MakeList(obj);
+            */    
+               for (int i = 0; i < args.Length; i++)
+               {
+                   if(args[0] == "-input")
+                   {
+                       path = args[1];
+                       string json = File.ReadAllText(path);       //Reads the JSON file to a string
+                       var obj = JsonConvert.DeserializeObject<Henkilo[]>(json);
+                       MakeList(obj);
+                       if(args.Length == 2)
+                       {
+                           PrintEveryOne(henkiloLista);
+                           break;
+                       }
+
+                       if(args[2] == "-sort" && args[3] == "age")
+                       {
+                           SortByAge(henkiloLista);
+                           break;
+                       }
+                       if (args[2] == "-sort" && args[3] == "lastname")
+                       {
+                           SortByLastName(henkiloLista);
+                           break;
+                       }
 
 
-            if (args[0] == "-input") { 
+
+                   } 
+
+               }   
+
+            /*
+            if (args[0] == "-input")
+            {
                 path = args[1];
                 string json = File.ReadAllText(path);       //Reads the JSON file to a string
                 var obj = JsonConvert.DeserializeObject<Henkilo[]>(json);
                 MakeList(obj);
-            }
-            
+                PrintEveryOne(obj);
 
-            if (args[2] == "-sort") {
-                if (args[3] == "age")
+
+
+                if (args[2] == "-sort" && args[3] == "age")
                     SortByAge(henkiloLista);
-                if (args[3] == "lastname")
+
+                if (args[2] == "-sort" && args[3] == "lastname")
                     SortByLastName(henkiloLista);
+
             }
+    */
+
             //if (args[0] == "-search")
-                    //searchcode
+            //searchcode
             //if (args[0] == "-findduplicateinfamilytree")
-                    //duplicatecode
-                     
+            //duplicatecode
 
-            //dynamic file = JsonConvert.DeserializeObject(json);       THIS did not work.
-            //Deserialisoi objektiin ja Henkilo luokkaan. Koska Json tiedosto oli Arraymuodossa tarvitsin muodon Henkilo[]
-
-
-
-
-           // PrintEveryOne(obj);
-            //Console.WriteLine("---------------------");
-          //  SortByAge(henkiloLista);
-            //Console.WriteLine("---------------------");
-            //Console.WriteLine(henkiloLista.Count);
-            //Console.WriteLine("---------------------");
-            //SortByLastName(henkiloLista);
-            
-            /*
-            
-            
-            for(int i = 0; i <obj.Length;i++ )
-            {
-                Console.WriteLine(obj[i].FirsName);
-                if (obj[i].Children != null)
-                {
-                    foreach (var ob in obj[i].Children)
-                    {
-                        Console.WriteLine(ob.FirsName);
-
-                        if (ob.Children != null)
-                        {
-                            foreach (var obb in ob.Children)
-                            {
-                                Console.WriteLine(obb.FirsName);
-
-                                if(obb.Children != null)
-                                {
-                                    foreach(var obbb in obb.Children)
-                                    {
-                                        Console.WriteLine(obbb.FirsName);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } 
-
-            }
-
-                */
 
         }
 
     public static void PrintEveryOne(dynamic json)
     {
-        
+            int counter = 0;
                 foreach (var obj in json)
                 {
                     Console.WriteLine(obj.FirsName + " " + obj.LastName + " " + obj.Age);
+                counter++;
+                if (counter == 10 || counter == 20 || counter == 30 || counter == 40)
+                {
+                    Console.WriteLine("Press Enter to show next  values");
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                        continue;
 
-                 if (obj.Children != null)
+                }
+
+                if (obj.Children != null)
                         PrintEveryOne(obj.Children);
+
                 }            
         }
 
     public static void SortByAge(List<Henkilo> list)
         {
             List<Henkilo> sortedByAgeList = list.OrderBy(o => o.Age).ToList();
+            int counter = 0;
             foreach(var i in sortedByAgeList)
             {
                 Console.WriteLine(i.FirsName + " " +  i.LastName + " "+ i.Age);
+                counter++;
+                if (counter == 10 || counter == 20 || counter == 30 || counter == 40)
+                {
+                    Console.WriteLine("Press Enter to show next  values");
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                        continue;
+
+
+                }
             }
         }
 
     public static void SortByLastName(List<Henkilo> list)
         {
             List<Henkilo> sortedByLastname = list.OrderBy(o => o.LastName).ToList();
-            foreach(var i in sortedByLastname)
+            int counter = 0;
+
+            foreach (var i in sortedByLastname)
             {
                 Console.WriteLine(i.FirsName + " " + i.LastName + " " + i.Age);
+                counter++;
+                if (counter == 10 || counter == 20 || counter == 30 || counter == 40)
+                {
+                    Console.WriteLine("Press Enter to show next  values");
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                        continue;
+
+
+                }
             }
         }
 
@@ -131,21 +150,42 @@ namespace familytreebrowser
         {
             foreach (var obj in json)
             {
-
-
                 // While looping through each object, save objects to a list using a constructor
                 henkiloLista.Add(new Henkilo(obj.FirsName, obj.LastName, obj.Gender, obj.Age));
 
                 if (obj.Children != null)
                     MakeList(obj.Children);
+            }
 
+        }
+
+
+        public static void PrintTen(List<Henkilo> list)
+        {
+            int counter = 0;
+            foreach (var i in list)
+            {
+                Console.WriteLine(i.FirsName + " " + i.LastName + " " + i.Age);
+                counter++;
+
+
+                if (counter == 10 || counter == 20 || counter == 30 || counter == 40)
+                {
+                    Console.WriteLine("Press Enter to show next 10 values");
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                        continue;
+
+
+                }
             }
 
         }
 
 
 
+
     }
+
     public class Henkilo
     {
         public string FirsName { get; set; }
